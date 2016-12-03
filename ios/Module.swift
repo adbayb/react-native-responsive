d7b6cda1b7884b59fb02819c9f5b2dbf863fed0c
Ayoub ADIB <ayoub.development@gmail.com>
2016-12-04 00:44:58 +0100

    Added some comments for ios native modules system

--- ios/Module.swift
+++ ios/Module.swift
@@ -1,10 +1,11 @@
-// YourModule.swift
 import Foundation
 
+// Le décorateur @objc permet de rendre accessible une classe, méthode, variable... au runtime objective-c
+// On peut modifier le nom de la classe,... exposé par ce décorateur via le premier argument du décorateur @obj (@obj(name_override))
 @objc(Module)
 class Module: NSObject {
 
     @objc func YouMethod(arg1: String, arg2: Bool) -> Void {
-        // --- do something
+        // todo
     }
 }
--- ios/RCTModule.h
+++
@@ -1,8 +0,0 @@
-// YourModule-Bridging-Header.h
-
-#ifndef ModuleBridge_h
-#define ModuleBridge_h
-#import "RCTBridge.h"
-#import "RCTBridgeModule.h"
-#import "RCTEventDispatcher.h"
-#endif /* YourModule_Bridging_Header_h */
